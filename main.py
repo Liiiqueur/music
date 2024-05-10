938ffba5f660841d8dcf33680b3ac1f1d6ab1533
Liiiqueur <154396703+Liiiqueur@users.noreply.github.com>
2024-05-10 16:32:03 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests
 
 API_KEY = '3165ed803092cb7d6e1087d1a389c45e'
-API_URL = 'https://liiiqueur.github.io/music/main.html?token=o7nI59R39ELw5VUrNkaROJCg8656v-kj'
+API_URL = 'http://ws.audioscrobbler.com/2.0/'
 
 # 예제: 사용자의 최근 듣기 기록 가져오기
 params = {
     'method': 'user.getRecentTracks',
-    'user': 'YOUR_USERNAME',
+    'user': 'geonwoo2473',
     'api_key': API_KEY,
     'format': 'json'
 }
